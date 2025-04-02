50bf4383084e9f02fdf3f1084c40838a14aa71ab
HK <35353525+Readtt@users.noreply.github.com>
2025-04-02 14:05:04 -0400

    fix lint errors

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -25,7 +25,7 @@ function Progress({
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
         className={cn("bg-primary h-full w-full flex-1 transition-all", bgColor)}
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
       />
     </ProgressPrimitive.Root>
   )
