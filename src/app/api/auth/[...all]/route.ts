d1404bf68bb9eb4e62050d21ae7044cdff38684b
HK <35353525+Readtt@users.noreply.github.com>
2025-04-02 14:17:17 -0400

    remove maxDuration and opt into fluid compute

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,4 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "~/server/auth";
- 
-export const maxDuration = 30;
+
 export const { POST, GET } = toNextJsHandler(auth);
