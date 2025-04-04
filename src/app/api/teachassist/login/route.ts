36c6e6b8246633937cc9efdf87d31d5370a3fe51
HK <35353525+Readtt@users.noreply.github.com>
2025-04-03 23:42:17 -0400

    test modified ta login route

--- src/app/api/teachassist/login/route.ts
+++ src/app/api/teachassist/login/route.ts
@@ -1,21 +1,26 @@
-import makeFetchCookie from "fetch-cookie";
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { NextResponse } from "next/server";
 import type { z } from "zod";
 import { type LoginResponse, loginSchema } from "~/common/types/login";
 import { tryCatch } from "~/server/helpers";
 
-const fetchCookie = makeFetchCookie(fetch);
+//@ts-expect-error No type definition
+import { fetch as cookieFetch, CookieJar } from "node-fetch-cookies";
 
 export async function POST(req: Request) {
   try {
     const bodyRaw = (await req.json()) as z.infer<typeof loginSchema>;
     const body = loginSchema.parse(bodyRaw);
     const { studentId, password } = body;
-
     const URL = `https://ta.yrdsb.ca/live/index.php?username=${studentId}&password=${password}&submit=Login&subject_id=0`;
 
+    const cookieJar = new CookieJar();
+
     const loginResponse = await tryCatch(
-      fetchCookie(URL, {
+      cookieFetch(cookieJar, URL, {
         method: "POST",
         body: "credentials",
       }),
@@ -27,7 +32,7 @@ export async function POST(req: Request) {
         { status: 503 },
       );
 
-    const html = await tryCatch(loginResponse.data.text());
+    const html = await tryCatch((loginResponse.data as Awaited<ReturnType<typeof fetch>>).text());
     if (
       html.error ||
       [
