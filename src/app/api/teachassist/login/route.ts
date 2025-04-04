399d7de349cc4520eb7a9ce822bc28e36b90a2a8
HK <35353525+Readtt@users.noreply.github.com>
2025-04-04 00:37:29 -0400

    log fetch error

--- src/app/api/teachassist/login/route.ts
+++ src/app/api/teachassist/login/route.ts
@@ -1,52 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-import { type NextRequest, NextResponse } from "next/server";
+import makeFetchCookie from "fetch-cookie";
+import { NextResponse } from "next/server";
 import type { z } from "zod";
 import { type LoginResponse, loginSchema } from "~/common/types/login";
 import { tryCatch } from "~/server/helpers";
 
-//@ts-expect-error No type definition
-import { fetch as cookieFetch, CookieJar } from "node-fetch-cookies";
+const fetchCookie = makeFetchCookie(fetch);
 
-export async function OPTIONS(req: NextRequest) {
-  return new NextResponse(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  })
-}
-
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   try {
     const bodyRaw = (await req.json()) as z.infer<typeof loginSchema>;
     const body = loginSchema.parse(bodyRaw);
     const { studentId, password } = body;
+
     const URL = `https://ta.yrdsb.ca/live/index.php?username=${studentId}&password=${password}&submit=Login&subject_id=0`;
 
-    const cookieJar = new CookieJar();
-
     const loginResponse = await tryCatch(
-      cookieFetch(cookieJar, URL, {
+      fetchCookie(URL, {
         method: "POST",
         body: "credentials",
       }),
     );
 
-    if (loginResponse.error)
+    if (loginResponse.error) {
+      console.log(loginResponse.error);
       return NextResponse.json<LoginResponse>(
         { error: "Teachassist is currently unavailable" },
         { status: 503 },
       );
+    }
 
-    const html = await tryCatch(
-      (loginResponse.data as Awaited<ReturnType<typeof fetch>>).text(),
-    );
+    const html = await tryCatch(loginResponse.data.text());
     if (
       html.error ||
       [
